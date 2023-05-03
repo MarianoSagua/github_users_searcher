4617bd75e77dd79ff62d9b862bfd2da83bc555c7
MarianoSagua <marianosagua4343@gmail.com>
2023-05-03 03:09:18 -0300

    Commit 2

--- src/API/DataUser.js
+++ src/API/DataUser.js
@@ -3,6 +3,7 @@ export const getUsers = async (user)=>{
         const URL = `https://api.github.com/users/${user}`;
         const response = await fetch(URL);
         const data = await response.json();
+        
         return data;
     } catch (error) {
         console.log(error);
